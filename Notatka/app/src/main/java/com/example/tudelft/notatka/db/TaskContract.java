07599f2875093c89774557b01f8c3780758eab3e
Tomek <borejko.t@gmail.com>
2017-03-20 21:05:55 +0100

    ToDoList app with async task

---
+++ Notatka/app/src/main/java/com/example/tudelft/notatka/db/TaskContract.java
@@ -0,0 +1,20 @@
+package com.example.tudelft.notatka.db;
+
+import android.provider.BaseColumns;
+
+import com.example.tudelft.notatka.MainActivity;
+
+public class TaskContract {
+    public static final String DB_NAME = "tasksDb";
+    public static final int DB_VERSION = 1;
+
+
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+
+        public static final String COL_TASK_TITLE = "title";
+        public static final String COL_TASK_DATE = "data";
+    }
+
+}
