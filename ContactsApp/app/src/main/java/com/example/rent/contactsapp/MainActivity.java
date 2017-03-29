a6c8381f90332b5b1290d7fa9ce88da4f3c2ee34
Tomek <borejko.t@gmail.com>
2017-03-29 20:41:22 +0200

    content provider created

--- ContactsApp/app/src/main/java/com/example/rent/contactsapp/MainActivity.java
+++ ContactsApp/app/src/main/java/com/example/rent/contactsapp/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.rent.contactsapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,4 +25,60 @@ public class MainActivity extends AppCompatActivity {
         Cursor contactsContact = getContentResolver()
                 .query(ContactsContract.AUTHORITY_URI, null, null, null, null);
     }
+
+    private void requestPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS));
+
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CONTACTS},
+                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+        } else {
+            loadContacts();
+        }
+    }
+
+    private void loadContacts() {
+        getSupportLoaderManager().initLoader(1,null,this);
+        Cursor contactCursor = getContentResolver()
+                .query(ContactsContract.Data.CONTENT_URI,null,null,null,null);
+        Toast.makeText()
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this, ContactsContract.Data.CONTENT_URI,null,null,null,null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        Toast.makeText(this, "load finished", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+
 }
+
